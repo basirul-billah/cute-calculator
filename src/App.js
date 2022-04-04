fbc5f3dc2cc66d121e6c77bfbc49f79cb4740e39
Basirul Billah <basirulbillah@gmail.com>
2022-04-05 03:55:46 +0600

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Home from './components/Home/Home';
+import Reviews from './components/Reviews/Reviews';
+import Dashboard from './components/Dashboards/Dashboards';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/review' element={<Reviews />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
 import Header from '../Header/Header';
-import Reviews from '../Reviews/Reviews';
-import Dashboard from '../Dashboards/Dashboards';
-import Blogs from '../Blogs/Blogs';
-import About from '../About/About';
-import NotFound from '../NotFound/NotFound';
 
 const Home = () => {
     return (
         <div>
-            <h1>Best Calculator in the world!</h1>
             <Header></Header>
-            <Routes>
-                <Route path='/' element={<Home />}></Route>
-                <Route path='/home' element={<Home />}></Route>
-                <Route path='/review' element={<Reviews />}></Route>
-                <Route path='/dashboard' element={<Dashboard />}></Route>
-                <Route path='/blogs' element={<Blogs />}></Route>
-                <Route path='/about' element={<About />}></Route>
-                <Route path='*' element={<NotFound />}></Route>
-            </Routes>
         </div>
     );
 };
