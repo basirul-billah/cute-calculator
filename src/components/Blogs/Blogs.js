5830e42fd42f687c410e778b80780cba5f32f6fb
Basirul Billah <basirulbillah@gmail.com>
2022-04-05 22:28:38 +0600

    added customer review in home using hooks

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,31 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            blogs page
+
+            {/* explaining context api */}
+            <div className='grid grid-cols-1 justify-items-center bg-orange-200 p-4 rounded-lg mx-20 mt-5 text-left'>
+                <h1 className='text-xl font-bold'>What is context API?</h1>
+                <ul className='list-decimal'>
+                    <li>Context API is a React structure that is used to globally share data from component to components instead of prop drilling.</li>
+                    <li>This API saves time and is much easier than prop drilling. </li>
+                    <li>In order to use Context API React.createContext() function is used.</li>
+                    <li>This function returns a consumer and a provider.</li>
+                    <li>The Provider component provides state to its children components.</li>
+                    <li>The Consumer component is used by the child that needs to use the Context API.</li>
+                </ul>
+            </div>
+            {/* explaining semantic tag */}
+            <div className='grid grid-cols-1 justify-items-center bg-orange-200 p-4 rounded-lg mx-20 mt-5 text-left'>
+                <h1 className='text-xl font-bold'>What is semantic tag?</h1>
+                <ul className='list-decimal'>
+                    <li>Semantic tag is used to specify its meaning to browsers and developers</li>
+                    <li>Examples of semantic tags are header, footer, nav, form, table, article etc.</li>
+                    <li>Semantic tags describes the information it holds within itself.</li>
+                    <li>This makes code much more readable and understandable to the developer.</li>
+                    <li>Semantic tags assists browser and web crawlers to classify and identify key parts of the web page.</li>
+                    <li>They are also beneficial for SEO.</li>
+                </ul>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,14 @@
 import { Link } from "react-router-dom";
-import useReview from "../../hooks/useReview";
+import useReview from "../../hooks/useReview"
 import ReviewPost from "../ReviewPost/ReviewPost";
 
 const Home = () => {
 
     const [reviews] = useReview();
 
-    const handleShowAllReview = () => {
-        <Link className='p-2 nav-links' to="/reviews">Reviews</Link>
-    }
-
     return (
         <div>
+
             {/* banner section */}
             <div className='flex justify-evenly'>
                 <div className='p-10 text-left'>
@@ -29,16 +26,21 @@ const Home = () => {
                 </div>
                 <img className='h-3/4' src="https://media.istockphoto.com/photos/yellow-calculator-picture-id1320223302?b=1&k=20&m=1320223302&s=170667a&w=0&h=k7wjCs4uEgr_SLN1mADslczDoQolMf0D-1T8V04TBOU=" alt="" />
             </div>
+            
             {/* review section */}
             <div>
                 <h1 className='text-3xl'>Customer Reviews</h1>
-                {
-                    reviews.map(review => <ReviewPost
-                        key = {review.id}
-                        review = {review}
-                    ></ReviewPost>)
-                }
-                <button onClick={() => handleShowAllReview} className="px-5 py-3 text-white m-4 bg-green-700 rounded-lg	">See all reviews</button>
+                <div className="grid grid-cols-3 gap-5 mx-10 mt-5">
+                    {
+                        reviews.map(review => <ReviewPost
+                            key={review.id}
+                            review={review}
+                        ></ReviewPost>)
+                    }
+                </div>
+                <Link to="/reviews">
+                    <button className="px-5 py-3 text-white m-4 bg-green-700 rounded-lg	">See all reviews</button>
+                </Link>
             </div>
         </div>
     );
